aa923a72930e6449b32611417ed05b8cf71c41a9
isadoratavare85@gmail.com <isadoratavare85@gmail.com>
2021-04-23 15:26:48 -0300

    Enviando atividade 2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:projectdefault/main.dart';
+import 'package:project3/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
